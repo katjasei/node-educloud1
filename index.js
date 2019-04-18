46a8c1dacbf3c8709dffd5845a7042df3bb10b4e
Katja Seikkinen <katjasei@metropolia.fi>
2019-04-18 09:41:51 +0300

    try to connect to database

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 'use strict';
 
+require('dotenv').config();
+
 console.log("hello world");
 
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
 
 app.get('/' ,(req,res)=> {
-
-  res.send('Hello World');
+  // simple query
+  connection.query(
+      'SELECT * FROM animals ORDER  BY name',
+      (err, results, fields) => {
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+        res.send(results);
 });
 
 app.post('/',
